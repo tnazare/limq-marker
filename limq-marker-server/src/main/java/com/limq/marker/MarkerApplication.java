af2648fa2563e9cc518f3a0fc68c0db55bb7a1ca
Marie Beaulieu <mabeaulieu@coveo.com>
2023-09-24 11:09:16 -0400

    Hello world app, pom and stuff

---
+++ limq-marker-server/src/main/java/com/limq/marker/MarkerApplication.java
@@ -0,0 +1,13 @@
+package com.limq.marker;
+
+/**
+ * Hello world!
+ *
+ */
+public class MarkerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
