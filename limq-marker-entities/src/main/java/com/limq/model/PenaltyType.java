2ffaf19d8e8edd6ecc84e0005b32a7503a2be011
Marie Beaulieu <mabeaulieu@coveo.com>
2024-01-07 08:44:42 -0500

    Fix Hamming

--- limq-marker-entities/src/main/java/com/limq/model/PenaltyType.java
+++ limq-marker-entities/src/main/java/com/limq/model/PenaltyType.java
@@ -3,7 +3,7 @@ package com.limq.model;
 public enum PenaltyType {
     FAILURE_TO_COMPLY_TO_THE_CARD("Non-respect de la carte"),
     GAME_DELAY("Retard de jeu"),
-    HAM_ACTING("Cabotinage"),
+    HAMMING("Cabotinage"),
     ILLEGAL_PROCEDURE("Procédure illégale"),
     OTHER("To be added");
 
